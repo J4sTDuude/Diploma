2cac98e47958cde275dcf1df40ac46e15255f7fb
J4sTDuude <yur4uk2012@gmail.com>
2022-02-09 17:44:50 +0200

    create post

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
 
+import { getPosts } from './actions/posts.js'
 import Posts from './components/Posts/Posts.js'
 import Form from './components/Form/Form.js'
-
 import logo from './images/logo.png'
+import useStyles from './styles.js'
+
 const App = () => {
+    const classes = useStyles()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getPosts)
+    }, [dispatch])
+
     return (
         <Container maxWidth="lg">
-            <AppBar position='static' color='inherit'>
-                <Typography variant='h2' align='center'>Comicses</Typography>
-                <img src={logo} alt='comicses' height='60'/>
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>Comicses</Typography>
+                <img className={classes.image} src={logo} alt='comicses' height='60'/>
             </AppBar>
             <Grow in>
                 <Container>
--- server/index.js
+++ server/index.js
@@ -8,12 +8,12 @@ import postRoutes from './routes/posts.js'
 const app = express();
 
 // http://localhost:5000/posts
-app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
 
+app.use('/posts', postRoutes)
 const CONNECTION_URL = 'mongodb+srv://J4sT:5609@cluster0.shsai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
