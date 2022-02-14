af9edb907347059ab5bcdc05ec02d3d53153e943
J4sTDuude <yur4uk2012@gmail.com>
2022-02-14 15:51:50 +0200

    Add Update function and fix some problems

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,8 +1,8 @@
-import * as api from '../api'
+import * as api from '../api/index.js'
 
 export const getPosts = () => async (dispatch) => {
     try {
-        const { data } = await api.fetchPosts
+        const { data } = await api.fetchPosts()
 
         dispatch({type: 'FETCH_ALL', payload: data}) 
     } catch (error) {
@@ -21,3 +21,13 @@ export const createPost = (post) => async (dispatch) => {
         console.log(error)
     }
 }
+
+export const updatePost = (id, post) => async(dispatch) => {
+    try {
+        const {data} = await api.updatePost(id, post)
+
+        dispatch({type: 'UPDATE', payload: data})
+    } catch (error) {
+        console.log(error)
+    }
+} 
